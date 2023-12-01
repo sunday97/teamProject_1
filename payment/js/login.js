2d5c586d6aa111c99eaea51a55356840bc720874
sunday97 <a01029243700@gmail.com>
2023-12-01 16:40:18 +0900

    login 스크립트작업 완료

--- payment/js/login.js
+++ payment/js/login.js
@@ -25,11 +25,54 @@ const loginBtn = document.querySelector(".login-btn");
 const registerBtn = document.querySelector(".register-btn");
 
 loginBtn.addEventListener("click", function () {
-  isVaildId(idInput.value);
+  isVaildId(idInput).then((e) => {
+    if (e) {
+      if (e == true) {
+        isVaildPw(idInput, pwInput).then((f) => {
+          if (f == true) {
+            alert("로그인 성공");
+          } else {
+            alert("아이디 또는 비밀번호가 틀렸습니다.");
+          }
+        });
+      }
+    } else {
+      alert("아이디 또는 비밀번호가 틀렸습니다.");
+    }
+  });
 });
 
 async function isVaildId(e) {
-  const snapshot = await getDatasLogin(e);
+  const snapshot = await getDatas("user");
+  // console.log(snapshot);
+  const idArr = [];
+  const idPwArr = [];
+  snapshot.forEach((doc) => {
+    // console.log(doc.data());
+    const { email, id, password, name, tel } = doc.data();
+    // console.log(typeof id);
+    idArr.push(id);
+    idPwArr.push({ id: id, pw: password });
+  });
+  // console.log("pormise 내에서 idArr : " + idArr);
+  // console.log(idPwArr);
+  // console.log("pormise 내에서 검사결과 : " + idArr.includes(e.value));
+  // console.log(idPwArr.find((item) => item.id === e.value));
+  // const idPwObj = idPwArr.find((item) => item.id === e.value);
+  // console.log(idPwObj["pw"]);
+  return idArr.includes(e.value);
+  // var exsitBox = idArr.includes(userId);
+}
 
-  console.log(snapshot);
+async function isVaildPw(a, b) {
+  const snapshot = await getDatas("user");
+  const idPwArr = [];
+  snapshot.forEach((doc) => {
+    const { email, id, password, name, tel } = doc.data();
+    idPwArr.push({ id: id, pw: password });
+  });
+  const idPwObj = idPwArr.find((item) => item.id === a.value);
+  // console.log(idPwObj.pw);
+  // console.log(b.value);
+  return idPwObj.pw === b.value;
 }
